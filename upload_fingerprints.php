357047db71a53aab1ca35f96a1e60613fa016fac
Ramesh Adapa <rameshadapa32@gmail.com>
2017-12-28 09:00:02 +0530

    upload fingerprints error.

--- upload_fingerprints.php
+++ upload_fingerprints.php
@@ -8,15 +8,19 @@ if(isset($_POST['user_id']) &&
   $fingerPrint = $_POST['fingerbase64'];
   if($emailId != "" && $fingerPrint != "")
   {
-    if(update_student_fp())
+    if(update_student_fp($emailId, $fingerPrint))
     {
       echo "Student $emailId finger print updated successfully.<br />";
-      echo "<a herf='./UPLOADFINGERPRINTS.php'>Click</a> to add another student details.";
+      echo "<a href='./UPLOADFINGERPRINTS.php'>Click</a> to add another student details.";
     }
     else
     {
       echo "Some error while submitting student finger print.";
     }
+  }
+  else
+  {
+    echo 'Please privide valid user email and finger prints.<br /><a href="./UPLOADFINGERPRINTS.php">Click</a> to go back.';
   }
 }
 ?>
