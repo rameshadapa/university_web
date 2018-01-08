cdce5f1f60db113999db4801dde45b4cef1eeffe
Ramesh Adapa <rameshadapa32@gmail.com>
2018-01-08 23:51:26 +0530

    Mantra fingerprints header tag.

--- streams_show.php
+++ streams_show.php
@@ -15,9 +15,9 @@
 if(isset($_GET['resource'])) {
 $resource = $_GET['resource'];
 ?>
-<table width="100%" height="391" border="1" bgcolor="#000060">
+<table width="100%" height="480" border="1" bgcolor="#000060">
     <tr>
-        <th width="80%" scope="col">Streams</th>
+        <th width="80%" scope="col">E-Learning streams</th>
     </tr>
     <tr>
         <td align="center"><iframe width="100%" height="500" src="<?=$resource;?>" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe></td>
