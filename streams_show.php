53e2ffa2fcf31280a53530797df15570a3abdfdb
Ramesh Adapa <rameshadapa32@gmail.com>
2018-01-08 23:43:49 +0530

    e-learning video styles.

--- streams_show.php
+++ streams_show.php
@@ -20,7 +20,7 @@ $resource = $_GET['resource'];
         <th width="80%" scope="col">Streams</th>
     </tr>
     <tr>
-        <td><iframe width="800" height="500" src="<?=$resource;?>" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe></td>
+        <td align="center"><iframe width="100%" height="500" src="<?=$resource;?>" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe></td>
     </tr>
 </table>
 <?php
